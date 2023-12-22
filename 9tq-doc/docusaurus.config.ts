a93a126427fe4d465b21b9096444abdeac4e818a
Kevin <kevin@9troisquarts.com>
2023-12-22 16:12:54 +0100

    ci: Github pages

--- 9tq-doc/docusaurus.config.ts
+++ 9tq-doc/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://9troisquarts.github.io/lib-docs/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -17,6 +17,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: '9troisquarts', // Usually your GitHub org/user name.
   projectName: 'Librairies', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
